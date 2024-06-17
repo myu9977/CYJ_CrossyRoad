a3faeb3d7f73a9e99807d97d56c818534fef1afd
myu9977 <148519904+myu9977@users.noreply.github.com>
2024-06-17 13:32:08 +0900

    오브젝트풀로 자동차 생성(오류남)

--- Assets/6_Scripts/Road/CarSpawner.cs
+++ Assets/6_Scripts/Road/CarSpawner.cs
@@ -1,14 +1,33 @@
 using UnityEngine;
+using UnityEngine.Pool;
 
 public class CarSpawner : MonoBehaviour
 {
-    public GameObject carPrefab;
+    //public GameObject carPrefab;
+    public ObjectPool objectPool;
     public Transform spawnPoint;
+    public CarData[] carDataArray;
 
     void Start()
     {
-        GameObject carGO = Instantiate(carPrefab, spawnPoint.position, Quaternion.identity);
-        carGO.transform.SetParent(spawnPoint);
+        //GameObject carGO = Instantiate(carPrefab, spawnPoint.position, Quaternion.identity);
+        //carGO.transform.SetParent(spawnPoint);
+
+        SpawnCar();
+    }
+
+    private void SpawnCar()
+    {
+        // 랜덤하게 자동차 데이터를 선택
+        int randomIndex = Random.Range(0, carDataArray.Length);
+        CarData carData = carDataArray[randomIndex];
+
+        // 풀에서 자동차를 가져와 초기화
+        GameObject carGO = objectPool.SpawnFromPool("Car", spawnPoint.position, spawnPoint.rotation);
+        if (carGO != null)
+        {
+            carGO.GetComponent<CarObject>().Initialize(carData);
+        }
     }
 
 }
